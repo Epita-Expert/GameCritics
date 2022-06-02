a75310f2c478648fb71d60b91d4a005593a164c7
Nathan Podesta <nathan.podesta@icloud.com>
2022-06-02 15:03:25 +0200

    Initial Commit

---
+++ GameCritic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameCritic
+//
+//  Created by Nathan Podesta on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
